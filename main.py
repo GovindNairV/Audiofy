4ea8276f0ab4957a3c8bd1931acc57d5e43b8f94
Govind Nair V <govind.nair.v@gmail.com>
2022-08-24 19:56:23 +0530

    pyttsx3 testing
    tested the pyttsx3 module in linux for the first time

---
+++ main.py
@@ -0,0 +1,5 @@
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("this is a sample test to see how the tts works. my name is govind. i am currently using espeak synthesizer")
+engine.runAndWait()
+ 
