0df6ffd2bcfe12ebaf36a415a1a46ae6592ac3d7
Govind Nair V <govind.nair.v@gmail.com>
2023-02-03 15:26:30 +0530

    gtts vs pyttsx

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import pyttsx3
-engine = pyttsx3.init()
-engine.say("this is a sample test to see how the tts works. my name is govind. i am currently using espeak synthesizer")
-engine.runAndWait()
+from gtts import gTTS
+
+tts = gTTS('this is a sample test to see how the tts works. my name is govind. i am currently using espeak synthesizer')
+tts.save('hello.mp3')
+
+# engine = pyttsx3.init()
+# engine.say("this is a sample test to see how the tts works. my name is govind. i am currently using espeak synthesizer")
+# engine.runAndWait()
  
